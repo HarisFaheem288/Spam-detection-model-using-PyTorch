e63f2a951a41dbafba8fb9a2bce606ac07af1ecb
Muhammad Haris <harisfaheem288@gmail.com>
2024-08-20 12:44:54 +0500

    Update spamtask.py

--- spamtask.py
+++ spamtask.py
@@ -15,7 +15,7 @@ import nltk
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
-    nltk.download('punkt')
+    nltk.download('punkt_tab')
 try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
